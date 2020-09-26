eeec2d29bd025f2a0ca0d35e1b795e64c23c1f33
danyvarghese83 <57636833+danyvarghese83@users.noreply.github.com>
2020-09-26 12:45:03 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Row from './Row';
+import requests from './requests';
+import Banner from './Banner';
+import Nav from './Nav';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <Nav/>  
+    <Banner />
+    
+    <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}
+    isLargeRow/>
+    <Row title="Trending Now"fetchUrl={requests.fetchTrending}isLargeRow/>
+    <Row title="Top Rated"fetchUrl ={requests.fetchTopRated}isLargeRow/>
+    <Row title="Action Movies"fetchUrl={requests.fetchActionMovies}isLargeRow/>
+    <Row title="Comedy Movies"fetchUrl={requests.fetchComedyMovies}isLargeRow/>
+    <Row title="Horror Movies"fetchUrl={requests.fetchHorrorMovies}isLargeRow/>
+    <Row title="Romantic Movies"fetchUrl={requests.fetchRomanceMovies}isLargeRow/>
+    <Row title="Documentaries"fetchUrl={requests.fetchDocumentaries}isLargeRow/>
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Nav.css';
+
+function Nav() {
+    return (
+        <div className="nav">
+            <img className="nav_logo"
+            src="https://fontmeme.com/permalink/200910/2d726b104d24042a20e0999b1303cb7e.png"
+            alt="Bonesflix logo"
+            />
+            
+        </div>
+    )
+}
+
+export default Nav
